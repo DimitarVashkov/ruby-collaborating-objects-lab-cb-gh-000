3204ad9f263b4a12474b988a3263f34067e40cea
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-03-26 22:02:34 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,5 +1,6 @@
 class MP3Importer
-  def import(list_of_filenames)
-    list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
+  attr_reader :path
+  def initialize(path)
+    @path = path
   end
 end
