1e7c4201d1c1e9aff87bf53099849cc36046f889
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-03-26 22:33:04 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,8 @@ class Song
   end
 
   def artist=(artist)
-    @artist = artist
+    self.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
   end
 
   def self.new_by_filename(file)
