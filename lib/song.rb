aeb41c39906aa49385b9766922011fb911a01c24
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-03-26 22:20:37 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,6 +12,7 @@ class Song
 
   def self.new_by_filename(file)
     input = file.split(' - ')
+    puts input
     song = Song.new(input[1])
     artist = Artist.find_or_create_by_name(input[0])
     song.artist = artist
